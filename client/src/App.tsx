b40a6e4186eee48f1cf8e1e1f3b3cb9127b7c7cc
zachmc27 <dzdudes1213@gmail.com>
2025-05-15 22:54:39 -0500

    added github workflow files, added linter to package.json, added blank home.tsx, cleaned up main.tsx and app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -39,11 +38,9 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
         <div className="container">
           <Outlet />
         </div>
-        <Footer />
       </div>
     </ApolloProvider>
   );
