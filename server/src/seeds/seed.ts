5d61f6a8b9c827568af73ab8d69cdc3c0a54c843
Bread <brett.mcbryde@yahoo.com>
2025-05-17 11:38:59 -0500

    added seed file for pet_breed_types

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,12 +1,14 @@
 import db from '../config/connection.js';
+import createTypes from './seed_types.js';
 
-import cleanDB from './cleanDB.js';
+// import cleanDB from './cleanDB.js';
 
 
 const seedDatabase = async (): Promise<void> => {
   try {
     await db();
-    await cleanDB();
+    // await cleanDB();
+    await createTypes();
 
     // await User.create(userData);
     console.log('Seeding completed successfully!');
