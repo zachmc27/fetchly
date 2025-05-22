a417b7610890bf013b9a90a834e81c7073f846da
Sammi Teel <sammijteel@gmail.com>
2025-05-22 13:30:12 -0500

    Comment reset home page to blank

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,37 +1,9 @@
 // Feed component that takes an array of post objects to rendeer on the page
-import Comments from "../components/Reusables/Comments";
 
-const commentsData = [{
-    id: 1,
-    user: "Alice",
-    comment: "This is a great post!",
-    likeCount: 5,
-    postedTime: new Date(),
-    replies: [
-      {
-        id: 5,
-        user: "Bob",
-        comment: "I agree with Alice!",
-        likeCount: 2,
-        postedTime: new Date(),
-        replies: []
-      }
-    ]
-  },
-  {
-    id: 2,
-    user: "Charlie",
-    comment: "Thanks for sharing.",
-    likeCount: 1,
-    postedTime: new Date(),
-    replies: []
-
-}];
 
 export default function Home() {
   return (
     <div>
-      <Comments comments={commentsData}/>
     </div>
   )
 }
