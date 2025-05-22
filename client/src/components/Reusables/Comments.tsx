099d39f35665f61ed12597e2110f8c7c4ea0f5fb
Sammi Teel <sammijteel@gmail.com>
2025-05-21 22:10:48 -0500

    Adding form and comment functionality

--- client/src/components/Reusables/Comments.tsx
+++ client/src/components/Reusables/Comments.tsx
@@ -3,8 +3,58 @@
 
 // utilize bubble button components to make the replies button and like button
 
-export default function Comments() {
+//import React, { useState } from 'react';
+import UserPlaceHolder from "../../assets/react.svg";
+import "../../SammiReusables.css";
+
+
+type Comment = {
+  user: string,
+  avatar?: string,
+  comment: string,
+  likeCount: number,
+  postedTime: Date,
+  replies: Comment[]
+}
+
+type CommentsProps = {
+  comments: Comment[];
+};
+
+function CommentItem({ comment }: { comment: Comment }) {
   return (
-    <div>Comments</div>
-  )
+    <div >
+      <div className="comment-container">
+        <div className="comment-row">
+          <div className="comment-img">
+            <img src={comment.avatar || UserPlaceHolder}></img> 
+          </div>
+          <div className="comment-content">
+            <span>{comment.user}</span> <br></br>
+            <span>{comment.comment}</span><br></br>
+            <div className="comment-icon-row">
+              <img src={UserPlaceHolder}></img>
+              <small className="sub">{comment.likeCount}</small>
+              <img src={UserPlaceHolder}></img>
+              <small className="sub">{comment.replies.length}</small>
+
+            </div>
+        </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default function Comments({ comments }: CommentsProps) {
+  comments.sort((a, b) => a.postedTime.getTime() - b.postedTime.getTime());
+  console.log(comments);
+  return (
+    <div>
+      {comments.map((comment, idx) => (
+        <CommentItem key={idx} comment={comment} />
+      ))}
+    </div>
+  );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,35 @@
 // Feed component that takes an array of post objects to rendeer on the page
+import Comments from "../components/Reusables/Comments";
+
+const commentsData = [
+  {
+    user: "Alice",
+    comment: "This is a great post!",
+    likeCount: 5,
+    postedTime: new Date(),
+    replies: [
+      {
+        user: "Bob",
+        comment: "I agree with Alice!",
+        likeCount: 2,
+        postedTime: new Date(),
+        replies: []
+      }
+    ]
+  },
+  {
+    user: "Charlie",
+    comment: "Thanks for sharing.",
+    likeCount: 1,
+    postedTime: new Date(),
+    replies: []
+  }
+];
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Comments comments={commentsData} />
+    </div>
   )
 }
