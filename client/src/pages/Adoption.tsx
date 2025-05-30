a48081921bf2b601e46cdae240194276eb254d79
Liathor <dyermisha@gmail.com>
2025-05-29 20:20:56 -0400

    Updates
    Updates

--- client/src/pages/Adoption.tsx
+++ client/src/pages/Adoption.tsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import Feed from "../components/Reusables/Feed";
 import AdoptionFocus from "../components/Focus/AdoptionFocus";
-import { QUERY_ADOPTIONS, FILTERED_ADOPTIONS } from "../utils/queries";
-import { useQuery, useLazyQuery } from "@apollo/client";
+import { QUERY_ADOPTIONS } from "../utils/queries";
+import { useQuery } from "@apollo/client";
 import SearchBar from "../components/Reusables/SearchBar"
-import MediaUpload from "../components/Reusables/MediaUpload";
 import "../ZachTemp.css"
 
 export default function Adoption() {
@@ -13,7 +12,9 @@ export default function Adoption() {
     const [selectedAdoptionId, setSelectedAdoptionId] = useState<string | null>(null);
 
     const { loading, error, data } = useQuery(QUERY_ADOPTIONS);
-    const [getFilteredAdoptions, { data: filteredData, loading: filteredLoading }] = useLazyQuery(FILTERED_ADOPTIONS);
+    
+    
+    const [filteredAdoptions, setFilteredAdoptions] = useState<any[] | null>(null);
 
     function handleAdoptionClick(id: string) {
       setSelectedAdoptionId(id);
@@ -27,14 +28,15 @@ export default function Adoption() {
     }
     
     function filterByDogs() {
-      getFilteredAdoptions({ variables: { type: "Dog" } });
+      if(!data?.adoptions) return;
+      const dogs = data.adoptions.filter(
+        (adoption: any) => adoptionPosts.pet?.type.type?.toLowerCase() === "dog"
+      );
     }
     
     function filterByCats() {
       getFilteredAdoptions({ variables: { type: "Cat" } });
     }
-
-    //function filterBySearch() {
     function filterBySearch() {
       getFilteredAdoptions();
     }
@@ -46,9 +48,6 @@ export default function Adoption() {
   return (
     <div className="adoption-page-container">
       <SearchBar send={filterBySearch} />
-      <MediaUpload onUpload={(media) => { 
-        console.log('Uploaded media:', media);
-      }} />
       <div className="filter-buttons-container">
         <button onClick={filterByAll} className="filter-button">All</button>
         <button onClick={filterByDogs} className="filter-button">Dogs</button>
