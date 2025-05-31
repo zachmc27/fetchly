5d59240c1ff2d6504bc5979c14d1f95b18552c01
Liathor <dyermisha@gmail.com>
2025-05-31 10:05:39 -0400

    sorted posts on home page by newest first

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,26 +1,20 @@
 import Feed from "../components/Reusables/Feed";
-// import { mockPostData } from "../mockdata/feed-data";
 // import NewMessage from "../components/Creators/NewMessage";
 import { QUERY_POSTS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
-//import { useState } from "react";
-
-// interface Post {
-//   id: string;
-//   poster: {
-//     refId: string;
-//     refModel: string;
-//   };
-//   contentText: string;
-// }
 
 export default function Home() {
 
   const { loading, error, data } = useQuery(QUERY_POSTS);
-  //const [filteredPosts, setFilteredPosts] = useState<Post[] | null>(null);
 
-  const posts = data?.posts;
-  console.log('Posts:', posts);
+  // Sort post by newest first
+  const posts = data?.posts
+    ? [...data.posts].sort(
+        (a, b) => Number(b.createdAt) - Number(a.createdAt)
+      )
+    : [];
+
+  console.log("Sorted Posts:", posts);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
