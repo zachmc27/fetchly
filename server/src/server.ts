59c97c2ef8d0b93fbeec854991df6181c41a9fad
Liathor <dyermisha@gmail.com>
2025-06-01 09:56:18 -0400

    Updated dirname path

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,12 +9,15 @@ import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
 import mongoose from 'mongoose';
 import { GridFSBucket, ObjectId } from 'mongodb';
-
+import { fileURLToPath } from 'url';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const startApolloServer = async () => {
   await server.start();
